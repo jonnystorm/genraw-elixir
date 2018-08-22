7ee9be462929fc18cc1d58fa90b27504b9b4021e
Jonathan D. Storm <jds@idio.link>
2018-08-22 18:38:40 -0500

    Restructure modules; add unlicense

--- lib/genraw.ex
+++ lib/genraw.ex
@@ -1,5 +1,13 @@
+# Copyright © 2018 Jonathan Storm <jds@idio.link> This work
+# is free. You can redistribute it and/or modify it under
+# the terms of the Do What The Fuck You Want To Public
+# License, Version 2, as published by Sam Hocevar. See the
+# COPYING.WTFPL file for more details.
+
 defmodule GenRaw do
   use GenServer
+
+  alias GenRaw.Utility
 
   defp procket_path,
     do: Application.get_env(:genraw_ex, :procket_path)
--- lib/genraw/helpers.ex
+++ lib/genraw/helpers.ex
@@ -1,4 +1,10 @@
-defmodule Helpers do
+# Copyright © 2018 Jonathan Storm <jds@idio.link> This work
+# is free. You can redistribute it and/or modify it under
+# the terms of the Do What The Fuck You Want To Public
+# License, Version 2, as published by Sam Hocevar. See the
+# COPYING.WTFPL file for more details.
+
+defmodule GenRaw.Helpers do
   def dix_header_format do
     """
      0                   1                   2                   3
